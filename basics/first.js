fcb42456d4126f2449807f08e98a1fec964e6f9f
Kunal-2017 <106303693+Kunal-2017@users.noreply.github.com>
2023-12-12 04:12:39 +0000

    product of 1 to 10

--- basics/first.js
+++ basics/first.js
@@ -1,7 +1,16 @@
-console.log("Hello world");
-let x,y;
-x=10;
-y=20;
-console.log(`sum of ${x} and ${y} is ${x+y}`);
-console.log(typeof x);
-console.log(typeof String(x));
+// console.log("Hello world");
+// let x,y;
+// x=10;
+// y=20;
+// console.log(`sum of ${x} and ${y} is ${x+y}`);
+// console.log(typeof x);
+// console.log(typeof String(x));
+// let a=prompt("Enter a number");
+// alert("My name is Kunal Raj");
+// document.write("My name is Kunal Raj");
+let p=1;
+for(let a=1;a<=10;a++)
+{
+p=p*a;
+}
+console.log(`Product of 1 to 10 is ${p}`);
